aae9d3cb914e8eaa37d65998c5b0dfa91dd62e11
GitChrisLopez <sabine.fitch252379@potros.itson.edu.mx>
2026-04-06 14:28:55 -0700

    Mock prenda para registro diario

--- ClosetVirtualAndroid/app/build.gradle.kts
+++ ClosetVirtualAndroid/app/build.gradle.kts
@@ -48,6 +48,7 @@ dependencies {
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.work.runtime.ktx)
     implementation("io.coil-kt:coil-compose:2.6.0")
+    implementation(libs.androidx.compose.foundation)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
---
+++ ClosetVirtualAndroid/app/src/main/java/ramirez/ruben/closetvirtual/feature/registrodiario/ui/RegistroDiarioScreen.kt
@@ -0,0 +1,58 @@
+package ramirez.ruben.closetvirtual.feature.registrodiario.ui
+
+import android.content.res.Configuration
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import ramirez.ruben.closetvirtual.R
+import ramirez.ruben.closetvirtual.data.PrendaMock
+import ramirez.ruben.closetvirtual.data.prendasMock
+import ramirez.ruben.closetvirtual.ui.theme.ClosetVirtualTheme
+
+@Composable
+fun RegistroDiarioScreen(onNavigateBack: () -> Unit = {}) {
+    var busqueda by remember { mutableStateOf("") }
+    // Estado para guardar las prendas (mock) que el usuario seleccione
+    val prendasSeleccionadas = remember { mutableStateListOf<Int>() }
+
+}
+
+
+@Preview(name = "Modo Claro", showBackground = true, showSystemUi = true)
+@Composable
+private fun PreviewModoClaro() {
+    ClosetVirtualTheme(darkTheme = false) {
+        RegistroDiarioScreen()
+    }
+}
+
+@Preview(
+    name = "Modo Oscuro",
+    showBackground = true,
+    showSystemUi = true,
+    uiMode = Configuration.UI_MODE_NIGHT_YES
+)
+@Composable
+private fun PreviewModoOscuro() {
+    ClosetVirtualTheme(darkTheme = true) {
+        RegistroDiarioScreen()
+    }
+}
