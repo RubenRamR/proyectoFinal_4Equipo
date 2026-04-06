b407f364d2462f998363c54d7927661fb6551d9d
RubenRamR <160176918+RubenRamR@users.noreply.github.com>
2026-04-06 11:56:04 -0700

    Creacion pantalla registro prenda. Add GestionPrendaScreen and update project files 👽
    Creacion de pantalla Registro prenda en modo claro y oscuro. Introduce a new UI screen for garment management (ClosetVirtualAndroid/app/src/main/java/.../GestionPrendaScreen.kt) and update app entrypoints and build config. Changes include: added multiple IDE config files under .idea (including deviceStreaming cache and markdown/misc settings), updated project-level .idea gradle.xml and misc.xml, modified app/build.gradle.kts, and adjusted MainActivity.kt to integrate the new feature. These updates add the new feature and align project configuration for development.

--- ClosetVirtualAndroid/app/src/main/java/ramirez/ruben/closetvirtual/MainActivity.kt
+++ ClosetVirtualAndroid/app/src/main/java/ramirez/ruben/closetvirtual/MainActivity.kt
@@ -11,20 +11,21 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import ramirez.ruben.closetvirtual.feature.gestionprenda.ui.GestionPrendaScreen
 import ramirez.ruben.closetvirtual.ui.theme.ClosetVirtualTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         enableEdgeToEdge()
+
         setContent {
             ClosetVirtualTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                GestionPrendaScreen(
+                    onNavigateBack = {
+                    }
+                )
             }
         }
     }
