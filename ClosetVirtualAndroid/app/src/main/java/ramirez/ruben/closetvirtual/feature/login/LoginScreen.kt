e28301449bef5be4bcc3539258a8a8a9bb8ddcb9
GitChrisLopez <sabine.fitch252379@potros.itson.edu.mx>
2026-04-08 14:49:35 -0700

    Agregue varios iconos en diferentes tamaños que ocuparemos para las pantallas de login y register, que ya fueron creadas y apenas se van a hacer (puro frontend)

---
+++ ClosetVirtualAndroid/app/src/main/java/ramirez/ruben/closetvirtual/feature/login/LoginScreen.kt
@@ -0,0 +1,28 @@
+package ramirez.ruben.closetvirtual.feature.login
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun LoginScreen(){
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    var isPasswordVisible by remember { mutableStateOf("") }
+}
