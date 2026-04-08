244b7559fa024c8152724f36ebefad1bdb49c60d
GitChrisLopez <sabine.fitch252379@potros.itson.edu.mx>
2026-04-08 16:22:57 -0700

    Pantalla de Login lista, aunque le faltan los mocks, falta pantalla de Registro y de Contraseña olvidada

--- ClosetVirtualAndroid/app/src/main/java/ramirez/ruben/closetvirtual/feature/login/LoginScreen.kt
+++ ClosetVirtualAndroid/app/src/main/java/ramirez/ruben/closetvirtual/feature/login/LoginScreen.kt
@@ -1,5 +1,6 @@
 package ramirez.ruben.closetvirtual.feature.login
 
+import android.content.res.Configuration
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -11,18 +12,208 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import ramirez.ruben.closetvirtual.feature.gestionprenda.ui.GestionPrendaScreen
+import ramirez.ruben.closetvirtual.ui.theme.ClosetVirtualTheme
+import ramirez.ruben.closetvirtual.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun LoginScreen(){
     var email by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
-    var isPasswordVisible by remember { mutableStateOf("") }
+    // Esto es para poder mostrar y ocultar la contrasena a gusto
+    var isPasswordVisible by remember { mutableStateOf(false) }
+
+    // Pantalla principal
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.background)
+            .padding(24.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Text(
+            text = "Login",
+            fontSize = 32.sp,
+            fontWeight = FontWeight.Bold,
+            color = MaterialTheme.colorScheme.onBackground,
+            modifier = Modifier.padding(top = 16.dp)
+        )
+
+        Spacer(modifier = Modifier.height(64.dp))
+
+        // Campos de texto de email y la contraseña
+        TextField(
+            value = email,
+            onValueChange = { email = it },
+            label = { Text("Email", color = Color.Gray) },
+            leadingIcon = {
+                Image(
+                    painter = painterResource(id = R.mipmap.email_icon),
+                    contentDescription = "Icono de email",
+                    modifier = Modifier.size(20.dp),
+                    colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.onSurface)
+                )
+            },
+
+            modifier = Modifier.fillMaxWidth(),
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+            colors = TextFieldDefaults.colors(
+                focusedContainerColor = Color.Transparent,
+                unfocusedContainerColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.LightGray,
+                focusedIndicatorColor = MaterialTheme.colorScheme.primary
+            )
+        )
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        TextField(
+            value = password,
+            onValueChange = { password = it },
+            label = { Text("Password", color = Color.Gray) },
+            leadingIcon = {
+                Image(
+                    painter = painterResource(id = R.mipmap.password_icon),
+                    contentDescription = "Icono de contraseña",
+                    modifier = Modifier.size(20.dp),
+                    colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.onSurface)
+                )
+            },
+
+            // Mecanica para poder mostrar y ocultar la contrasena
+            trailingIcon = {
+                IconButton(onClick = { isPasswordVisible = !isPasswordVisible }) {
+                    Image(
+                        painter = painterResource(
+                            id =
+                                if (isPasswordVisible) R.mipmap.nohidden_icon
+                                else R.mipmap.hidden_icon
+                        ),
+                        contentDescription =
+                            if (isPasswordVisible) "Ocultar contraseña"
+                            else "Mostrar contraseña",
+                        modifier = Modifier.size(20.dp),
+                        colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.onSurface)
+                    )
+                }
+            },
+
+            modifier = Modifier.fillMaxWidth(),
+            singleLine = true,
+            visualTransformation =
+                if (isPasswordVisible) VisualTransformation.None
+                else PasswordVisualTransformation(),
+
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+
+            colors = TextFieldDefaults.colors(
+                focusedContainerColor = Color.Transparent,
+                unfocusedContainerColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.LightGray,
+                focusedIndicatorColor = MaterialTheme.colorScheme.primary
+            )
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Text(
+            text = "Forgot password?",
+            color = Color(0xFF1E88E5),
+            fontSize = 14.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+                .align(Alignment.End)
+                .clickable { println("Usuario Contraseña olvidada") }
+        )
+
+        Spacer(modifier = Modifier.height(48.dp))
+
+        Button(
+            onClick = { println("Iniciar sesion con: Email=$email, Password=$password") },
+            modifier = Modifier.fillMaxWidth(),
+            shape = RoundedCornerShape(8.dp),
+            colors = ButtonDefaults.buttonColors
+                (containerColor = MaterialTheme.colorScheme.primary)
+        ) {
+            Text(
+                text = "Login",
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.White
+            )
+        }
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        Text(
+            text = "--------------------------- or ---------------------------",
+            color = Color.Gray,
+            fontSize = 16.sp
+        )
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        Button(
+            onClick = { println("Redireccionar al usuario a RegisterScreen") },
+            modifier = Modifier.fillMaxWidth(),
+            shape = RoundedCornerShape(8.dp),
+            colors = ButtonDefaults.buttonColors
+                (containerColor = MaterialTheme.colorScheme.tertiary)
+        ) {
+            Text(
+                text = "Sign up",
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Bold,
+                color = MaterialTheme.colorScheme.onTertiary
+            )
+        }
+
+        Spacer(modifier = Modifier.height(90.dp))
+
+        Box(modifier = Modifier.fillMaxWidth(), contentAlignment = Alignment.Center) {
+            Image(
+                painter = painterResource(id = R.mipmap.fingerprint),
+                contentDescription = "Iniciar sesión con huella",
+                modifier = Modifier
+                    .size(100.dp)
+                    .clickable { println("Inicio de sesion fon Fingerprint") }
+            )
+        }
+
+        Spacer(modifier = Modifier.weight(1f))
+
+    }
+}
+
+@Preview(name = "Modo Claro", showBackground = true, showSystemUi = true)
+@Composable
+private fun PreviewModoClaro() {
+    ClosetVirtualTheme(darkTheme = false) {
+        LoginScreen()
+    }
+}
+
+@Preview(
+    name = "Modo Oscuro",
+    showBackground = true,
+    showSystemUi = true,
+    uiMode = Configuration.UI_MODE_NIGHT_YES
+)
+@Composable
+private fun PreviewModoOscuro() {
+    ClosetVirtualTheme(darkTheme = true) {
+        LoginScreen()
+    }
 }
--- ClosetVirtualAndroid/app/src/main/java/ramirez/ruben/closetvirtual/feature/signup/RegisterScreen.kt
+++ ClosetVirtualAndroid/app/src/main/java/ramirez/ruben/closetvirtual/feature/signup/RegisterScreen.kt
@@ -1,5 +1,6 @@
 package ramirez.ruben.closetvirtual.feature.signup
 
+import android.content.res.Configuration
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -16,8 +17,11 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import ramirez.ruben.closetvirtual.feature.login.LoginScreen
+import ramirez.ruben.closetvirtual.ui.theme.ClosetVirtualTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -25,4 +29,27 @@ fun RegisterScreen(){
     var email by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
     var isPasswordVisible by remember { mutableStateOf("") }
+
+
+}
+
+@Preview(name = "Modo Claro", showBackground = true, showSystemUi = true)
+@Composable
+private fun PreviewModoClaro() {
+    ClosetVirtualTheme(darkTheme = false) {
+        RegisterScreen()
+    }
+}
+
+@Preview(
+    name = "Modo Oscuro",
+    showBackground = true,
+    showSystemUi = true,
+    uiMode = Configuration.UI_MODE_NIGHT_YES
+)
+@Composable
+private fun PreviewModoOscuro() {
+    ClosetVirtualTheme(darkTheme = true) {
+        RegisterScreen()
+    }
 }
