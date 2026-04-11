a546574313b4f823a69d521206f22e84a13aef61
GitChrisLopez <sabine.fitch252379@potros.itson.edu.mx>
2026-04-11 15:31:21 -0700

    "Corrigiendo unos errores, CU finalizado"

--- ClosetVirtualAndroid/app/src/main/java/ramirez/ruben/closetvirtual/feature/calendario/ui/CalendarioScreen.kt
+++
@@ -1,2 +0,0 @@
-package ramirez.ruben.closetvirtual.feature.calendario.ui
-
--- ClosetVirtualAndroid/app/src/main/java/ramirez/ruben/closetvirtual/feature/registrodiario/ui/RegistroDiarioScreen.kt
+++ ClosetVirtualAndroid/app/src/main/java/ramirez/ruben/closetvirtual/feature/registrodiario/ui/RegistroDiarioScreen.kt
@@ -16,6 +16,7 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -79,22 +80,26 @@ fun RegistroDiarioScreen(onNavigateBack: () -> Unit = {}) {
             Image(
                 painter = painterResource(id = R.mipmap.coldstreak_icon),
                 contentDescription = "Racha inactiva",
-                modifier = Modifier.size(40.dp)
+                modifier = Modifier.size(40.dp),
+                colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.onBackground)
             )
             Image(
                 painter = painterResource(id = R.mipmap.coldstreak_icon),
                 contentDescription = "Racha inactiva",
-                modifier = Modifier.size(40.dp)
+                modifier = Modifier.size(40.dp),
+                colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.onBackground)
             )
             Image(
                 painter = painterResource(id = R.mipmap.coldstreak_icon),
                 contentDescription = "Racha inactiva",
-                modifier = Modifier.size(40.dp)
+                modifier = Modifier.size(40.dp),
+                colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.onBackground)
             )
             Image(
                 painter = painterResource(id = R.mipmap.coldstreak_icon),
                 contentDescription = "Racha inactiva",
-                modifier = Modifier.size(40.dp)
+                modifier = Modifier.size(40.dp),
+                colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.onBackground)
             )
         }
 
